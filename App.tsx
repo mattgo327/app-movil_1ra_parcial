f7090e98c56feeba27ff39397fbff5458d3964a9
glopeza <glopeza.3it@bancochile.cl>
2024-08-08 14:58:28 -0400

    clase04 react-native-elements

--- App.tsx
+++ App.tsx
@@ -1,32 +1,36 @@
 import React from 'react';
-
-import {
-  ScrollView,
-  StyleSheet,
-} from 'react-native';
+import {ScrollView,StyleSheet} from 'react-native';
 //Componentes que se van sumando a mi pantalla principal
 import BasicComponents from './src/components/BasicComponents';
 import TextComponents from './src/components/TextComponents';
 import TextInputComponents from './src/components/TextInputComponents';
 import ListNotasComponents from './src/components/ListNotasComponents';
+import Home from './src/react-native-elements/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AvatarBasic from './src/react-native-elements/AvatarBasic';
 
-
-
+const Stack = createNativeStackNavigator();
 
 const App = () =>{
   return (
-    <ScrollView>
-       {/* <BasicComponents></BasicComponents>*/}
-       {/* <TextComponents></TextComponents>*/}
-       {/* <TextInputComponents></TextInputComponents>*/}
-       <ListNotasComponents></ListNotasComponents>
-    </ScrollView>
+    //<ScrollView>
+      // {/* <BasicComponents></BasicComponents>*/}
+      // {/* <TextComponents></TextComponents>*/}
+      // {/* <TextInputComponents></TextInputComponents>*/}
+      // {/*<ListNotasComponents></ListNotasComponents>*/}
+       <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Notas" component={ListNotasComponents} />
+            <Stack.Screen name="Avatar" component={AvatarBasic} />
+          </Stack.Navigator>
+       </NavigationContainer>
+   // </ScrollView>
 
   );
 }
-
 const styles = StyleSheet.create({
-
 });
 
 export default App;
